9549417db97937b227f47188d3da173ec53634ec
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-03-20 23:53:53 +0500

    ui updated

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let resultBox = document.getElementById("result-box");
 let startButton = document.getElementById("start")
 let changeColor = ("click", () => {
   let input = inputBox.value;
-  resultBox. style.backgroundColor = input;
+  resultBox.style.backgroundColor = input;
 });
 inputBox.addEventListener("input", changeColor);
 window.addEventListener("load", changeColor);
